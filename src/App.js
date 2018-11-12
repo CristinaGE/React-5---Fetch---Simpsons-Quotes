4773abf7b91e42f72868f9d6826c0bec708d1970
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2018-11-12 11:45:10 +0100

    fin quête 5 fetch

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import  GenerateQuote  from  './GenerateQuote';
+import  DisplayQuote  from  './DisplayQuote';
+
+const samplequote = {
+  "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
+  "character": "Nelson Muntz",
+  "image" : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+  "characterDirection" : "Left"
+};
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      quote: samplequote
+    };
+  }
+  getQuote(){
+    fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        quote: data[0],
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <GenerateQuote  selectQuote={() =>  this.getQuote()} />
+          <DisplayQuote quote={this.state.quote} />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
